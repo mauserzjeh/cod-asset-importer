b8a644dc3b0e7cc854ec616bf33ff0f06c95033d
mauserzjeh <mauserzjeh@gmail.com>
2021-02-08 23:37:46 +0100

    BaseEnum, FMT_CHAR constants

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,31 @@
 import struct
+
 from collections import namedtuple
 
-FMT_CHARS = [
-    'c', # char
-    'b', # signed char
-    'B', # unsigned char
-    'h', # short
-    'H', # unsigned short
-    'i', # int
-    'I', # unsigned int
-    'l', # long
-    'L', # unsigned long
-    'q', # long long
-    'Q', # unsigned long long
-    'f', # float
-    'd'  # double
-]
+class BaseEnum(type):
+    def __iter__(self):
+        _attr_values = []
+        _vars = vars(self)
+        for _name, _value in _vars.items():
+            if not callable(getattr(self, _name)) and not _name.startswith('__') and not _name.endswith('__'):
+                _attr_values.append(_value)
+        return iter(_attr_values)
+            
+
+class FMT_CHARS(metaclass = BaseEnum):
+    CHAR =                  'c' # char
+    SIGNED_CHAR =           'b' # signed char
+    UNSIGNED_CHAR =         'B' # unsigned char
+    SHORT =                 'h' # short
+    UNSIGNED_SHORT =        'H' # unsigned short
+    INTEGER =               'i' # int
+    UNSIGNED_INTEGER =      'I' # unsigned int
+    LONG =                  'l' # long
+    UNSIGNED_LONG =         'L' # unsigned long
+    LONG_LONG =             'q' # unsigned long
+    UNSIGNED_LONG_LONG =    'Q' # unsigned long long
+    FLOAT =                 'f' # float
+    DOUBLE =                'd' # double
 
 def read_fmt(file, fmt_str: str, namedtuple = False, fmt_byte_order: str = '<'):
     fmt = fmt_byte_order + fmt_str
@@ -35,44 +45,45 @@ def read_fmt(file, fmt_str: str, namedtuple = False, fmt_byte_order: str = '<'):
     return data_unpacked
 
 
+
 def read_char(file):
-    return read_fmt(file, 'c')
+    return read_fmt(file, FMT_CHARS.CHAR)
 
 def read_schar(file):
-    return read_fmt(file, 'b')
+    return read_fmt(file, FMT_CHARS.SIGNED_CHAR)
 
 def read_uchar(file):
-    return read_fmt(file, 'B')
+    return read_fmt(file, FMT_CHARS.UNSIGNED_CHAR)
 
 def read_short(file):
-    return read_fmt(file, 'h')
+    return read_fmt(file, FMT_CHARS.SHORT)
 
 def read_ushort(file):
-    return read_fmt(file, 'H')
+    return read_fmt(file, FMT_CHARS.UNSIGNED_SHORT)
 
 def read_int(file):
-    return read_fmt(file, 'i')
+    return read_fmt(file, FMT_CHARS.INTEGER)
 
 def read_uint(file):
-    return read_fmt(file, 'I')
+    return read_fmt(file, FMT_CHARS.UNSIGNED_INTEGER)
 
 def read_long(file):
-    return read_fmt(file, 'l')
+    return read_fmt(file, FMT_CHARS.LONG)
 
 def read_ulong(file):
-    return read_fmt(file, 'L')
+    return read_fmt(file, FMT_CHARS.UNSIGNED_LONG)
 
 def read_longlong(file):
-    return read_fmt(file, 'q')
+    return read_fmt(file, FMT_CHARS.LONG_LONG)
 
 def read_ulonglong(file):
-    return read_fmt(file, 'Q')
+    return read_fmt(file, FMT_CHARS.UNSIGNED_LONG_LONG)
 
 def read_float(file):
-    return read_fmt(file, 'f')
+    return read_fmt(file, FMT_CHARS.FLOAT)
 
 def read_double(file):
-    return read_fmt(file, 'd')
+    return read_fmt(file, FMT_CHARS.DOUBLE)
 
 def read_nullstr(file):
     string = b''
