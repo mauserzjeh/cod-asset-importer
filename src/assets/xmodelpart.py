8fcabe28d58ed3c1db20993a8b40ea4c9f5defe1
mauserzjeh <mauserzjeh@gmail.com>
2021-10-21 23:25:37 +0200

    viewhands bone positions

--- src/assets/xmodelpart.py
+++ src/assets/xmodelpart.py
@@ -24,6 +24,63 @@ class XModelPart:
         4: 'viewhands'
     }
     ROTATION_DIVISOR = 32768.0
+    VIEWHANDS_BONE_POSITIONS = {
+        "tag_view":         mathutils.Vector((0.0,              0.0,            0.0)),
+        "tag_torso":        mathutils.Vector((-11.76486,        0.0,            -3.497466)),
+        "j_shoulder_le":    mathutils.Vector((2.859542,         20.16072,       -4.597286)),
+        "j_elbow_le":       mathutils.Vector((30.7185,          -8E-06,         3E-06)),
+        "j_wrist_le":       mathutils.Vector((29.3906,          1.9E-05,        -3E-06)),
+        "j_thumb_le_0":     mathutils.Vector((2.786345,         2.245192,       0.85161)),
+        "j_thumb_le_1":     mathutils.Vector((4.806596,         -1E-06,         3E-06)),
+        "j_thumb_le_2":     mathutils.Vector((2.433519,         -2E-06,         1E-06)),
+        "j_thumb_le_3":     mathutils.Vector((3.0,              -1E-06,         -1E-06)),
+        "j_flesh_le":       mathutils.Vector((4.822557,         1.176307,       -0.110341)),
+        "j_index_le_0":     mathutils.Vector((10.53435,         2.786251,       -3E-06)),
+        "j_index_le_1":     mathutils.Vector((4.563,            -3E-06,         1E-06)),
+        "j_index_le_2":     mathutils.Vector((2.870304,         3E-06,          -2E-06)),
+        "j_index_le_3":     mathutils.Vector((2.999999,         4E-06,          1E-06)),
+        "j_mid_le_0":       mathutils.Vector((10.71768,         0.362385,       -0.38647)),
+        "j_mid_le_1":       mathutils.Vector((4.842623,         -1E-06,         -1E-06)),
+        "j_mid_le_2":       mathutils.Vector((2.957112,         -1E-06,         -1E-06)),
+        "j_mid_le_3":       mathutils.Vector((3.000005,         4E-06,          0.0)),
+        "j_ring_le_0":      mathutils.Vector((9.843364,         -1.747671,      -0.401116)),
+        "j_ring_le_1":      mathutils.Vector((4.842618,         4E-06,          -3E-06)),
+        "j_ring_le_2":      mathutils.Vector((2.755294,         -2E-06,         5E-06)),
+        "j_ring_le_3":      mathutils.Vector((2.999998,         -2E-06,         -4E-06)),
+        "j_pinky_le_0":     mathutils.Vector((8.613766,         -3.707476,      0.16818)),
+        "j_pinky_le_1":     mathutils.Vector((3.942609,         1E-06,          1E-06)),
+        "j_pinky_le_2":     mathutils.Vector((1.794117,         3E-06,          -3E-06)),
+        "j_pinky_le_3":     mathutils.Vector((2.83939,          -1E-06,         4E-06)),
+        "j_wristtwist_le":  mathutils.Vector((21.60379,         1.2E-05,        -3E-06)),
+        "j_shoulder_ri":    mathutils.Vector((2.859542,         -20.16072,      -4.597286)),
+        "j_elbow_ri":       mathutils.Vector((-30.71852,        4E-06,          -2.4E-05)),
+        "j_wrist_ri":       mathutils.Vector((-29.39067,        4.4E-05,        2.2E-05)),
+        "j_thumb_ri_0":     mathutils.Vector((-2.786155,        -2.245166,      -0.851634)),
+        "j_thumb_ri_1":     mathutils.Vector((-4.806832,        -6.6E-05,       0.000141)),
+        "j_thumb_ri_2":     mathutils.Vector((-2.433458,        -3.8E-05,       -5.3E-05)),
+        "j_thumb_ri_3":     mathutils.Vector((-3.000123,        0.00016,        2.5E-05)),
+        "j_flesh_ri":       mathutils.Vector((-4.822577,        -1.176315,      0.110318)),
+        "j_index_ri_0":     mathutils.Vector((-10.53432,        -2.786281,      -7E-06)),
+        "j_index_ri_1":     mathutils.Vector((-4.562927,        -5.8E-05,       5.4E-05)),
+        "j_index_ri_2":     mathutils.Vector((-2.870313,        -6.5E-05,       0.0001)),
+        "j_index_ri_3":     mathutils.Vector((-2.999938,        0.000165,       -6.5E-05)),
+        "j_mid_ri_0":       mathutils.Vector((-10.71752,        -0.362501,      0.386463)),
+        "j_mid_ri_1":       mathutils.Vector((-4.842728,        0.000151,       2.8E-05)),
+        "j_mid_ri_2":       mathutils.Vector((-2.957152,        -8.7E-05,       -2.2E-05)),
+        "j_mid_ri_3":       mathutils.Vector((-3.00006,         -6.8E-05,       -1.9E-05)),
+        "j_ring_ri_0":      mathutils.Vector((-9.843175,        1.747613,       0.401109)),
+        "j_ring_ri_1":      mathutils.Vector((-4.842774,        0.000176,       -6.3E-05)),
+        "j_ring_ri_2":      mathutils.Vector((-2.755269,        -1.1E-05,       0.000149)),
+        "j_ring_ri_3":      mathutils.Vector((-3.000048,        -4.1E-05,       -4.9E-05)),
+        "j_pinky_ri_0":     mathutils.Vector((-8.613756,        3.707438,       -0.168202)),
+        "j_pinky_ri_1":     mathutils.Vector((-3.942537,        -0.000117,      -6.5E-05)),
+        "j_pinky_ri_2":     mathutils.Vector((-1.794038,        0.000134,       0.000215)),
+        "j_pinky_ri_3":     mathutils.Vector((-2.839375,        5.6E-05,        -0.000115)),
+        "j_wristtwist_ri":  mathutils.Vector((-21.60388,        9.7E-05,        8E-06)),
+        "tag_weapon":       mathutils.Vector((38.5059,          0.0,            -17.15191)),
+        "tag_cambone":      mathutils.Vector((0.0,              0.0,            0.0)),
+        "tag_camera":       mathutils.Vector((0.0,              0.0,            0.0)),
+    }
 
     # --------------------------------------------------------------------------------------------
     class _bone_transform:
@@ -95,6 +152,11 @@ class XModelPart:
 
                     current_bone = self.bones[bone_index]
                     current_bone.name = bone_name
+
+                    if bone_name in self.VIEWHANDS_BONE_POSITIONS:
+                        viewmodel_position = self.VIEWHANDS_BONE_POSITIONS[bone_name]
+                        current_bone.local_transform.position = viewmodel_position / 2.54
+                        current_bone.world_transform.position = viewmodel_position / 2.54
 
                     if current_bone.parent > -1:
                         parent_bone = self.bones[current_bone.parent]
