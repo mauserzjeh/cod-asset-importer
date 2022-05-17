2f3ccf962244895a7399bd75383821819c9a674b
mauserzjeh <mauserzjeh@gmail.com>
2022-05-17 22:03:51 +0200

    remove individual iwi support

--- src/__init__.py
+++ src/__init__.py
@@ -6,8 +6,8 @@ bl_info = {
     "name": "Call of Duty Asset Importer",
     "description": "Import various Call of Duty assets",
     "author": "Soma Rádóczi",
-    "version": (0, 0, 1),
-    "blender": (2, 93, 0),
+    "version": (2, 0, 0),
+    "blender": (3, 0, 0),
     "location": "File > Import/Export",
     "category": "Import-Export",
     "warning": "This addon is still in development",
@@ -24,11 +24,6 @@ operators_list = (
         "text": "Call of Duty model (xmodel)",
         "function": None
     },
-    {
-        "class": operators.IWiImporter,
-        "text": "Call of Duty texture (iwi)",
-        "function": None
-    }
 )
 
 
--- src/addon/operators.py
+++ src/addon/operators.py
@@ -14,7 +14,7 @@ class D3DBSPImporter(bpy.types.Operator):
     filter_glob : bpy.props.StringProperty(default='*.d3dbsp', options={'HIDDEN'})
 
     def execute(self, context):
-        assetpath = os.path.abspath(os.path.join(os.path.join(self.filepath, os.pardir), os.pardir))
+        assetpath = os.path.abspath(os.path.join(self.filepath, os.pardir, os.pardir))
         importer.import_d3dbsp(assetpath, self.filepath)
         return {'FINISHED'}
 
@@ -29,31 +29,8 @@ class XModelImporter(bpy.types.Operator):
 
     filepath : bpy.props.StringProperty(subtype='FILE_PATH')
     def execute(self, context):
-        assetpath = os.path.abspath(os.path.join(os.path.join(self.filepath, os.pardir), os.pardir))
+        assetpath = os.path.abspath(os.path.join(self.filepath, os.pardir, os.pardir))
         importer.import_xmodel(assetpath, self.filepath, True)
-        return {'FINISHED'}
-
-    def invoke(self, context, event):
-        bpy.context.window_manager.fileselect_add(self)
-        return {'RUNNING_MODAL'}
-
-class IWiImporter(bpy.types.Operator):
-    bl_idname = 'cod_asset_importer.iwi_importer'
-    bl_label = 'Import'
-    bl_options = {'UNDO'}
-
-    filepath : bpy.props.StringProperty(subtype='FILE_PATH')
-    filename_ext = '.iwi'
-    filter_glob : bpy.props.StringProperty(default='*.iwi', options={'HIDDEN'})
-    normalmap : bpy.props.BoolProperty(
-        name = 'Normal map',
-        description = 'Whether the texture is a normal map or not.',
-        default = False
-    )
-
-    def execute(self, context ):
-        assetpath = os.path.abspath(os.path.join(os.path.join(self.filepath, os.pardir), os.pardir))
-        importer.import_texture(assetpath, self.filepath, self.normalmap)
         return {'FINISHED'}
 
     def invoke(self, context, event):
