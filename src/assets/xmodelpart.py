9a8949716ad61814b7df8e9484ea73d699a8148f
mauserzjeh <mauserzjeh@gmail.com>
2022-05-30 23:05:53 +0200

    cod4 xmodelsurf, xmodelpart

--- src/assets/xmodelpart.py
+++ src/assets/xmodelpart.py
@@ -346,3 +346,100 @@ class XModelPartV14:
         except:
             log.error_log(traceback.print_exc())
             return False
+
+"""
+XModelPartV25 represents xmodelpart structure for CoD4
+"""
+class XModelPartV25:
+    
+    PATH = 'xmodelparts'
+    VERSION = 25
+    ROTATION_DIVISOR = 32768.0
+
+    # --------------------------------------------------------------------------------------------
+    class _bone_transform:
+        __slots__ = ('rotation', 'position')
+        
+        def __init__(self, rotation: mathutils.Quaternion, position: mathutils.Vector) -> None:
+            self.rotation = rotation
+            self.position = position
+
+    class _bone:
+        __slots__ = ('name', 'parent', 'local_transform', 'world_transform')
+
+        def __init__(self, local_transform: XModelPartV25._bone_transform, parent: int = -1, name: str = '') -> None:
+            self.name = name
+            self.parent = parent
+            self.local_transform = local_transform
+            self.world_transform = copy.deepcopy(local_transform)
+
+        def generate_world_transform_by_parent(self, parent: XModelPartV25._bone) -> None:
+            self.world_transform.position = parent.world_transform.position + (parent.world_transform.rotation @ self.local_transform.position)        
+            self.world_transform.rotation = parent.world_transform.rotation @ self.local_transform.rotation
+
+    # --------------------------------------------------------------------------------------------
+
+    __slots__ = ('name', 'model_type', 'bones')
+
+    def __init__(self) -> None:
+        self.name = ''
+        self.model_type = 0
+        self.bones = []
+    
+    def load(self, xmodel_part: str) -> bool:
+        self.name = os.path.basename(xmodel_part)
+        self.model_type = self.name[-1]
+        try:
+            with open(xmodel_part, 'rb') as file:
+                header = file_io.read_fmt(file, '3H', collections.namedtuple('header', 'version, bone_count, root_bone_count'))
+                if header.version != self.VERSION:
+                    log.info_log(f'Xmodel version {header.version} is not supported!')
+                    return False
+
+                for _ in range(header.root_bone_count):
+                    rotation = mathutils.Quaternion()
+                    position = mathutils.Vector()
+                    bone_transform = self._bone_transform(rotation, position)
+                    bone = self._bone(bone_transform)
+                    self.bones.append(bone)
+
+                for _ in range(header.bone_count):
+
+                    # read raw bone data
+                    raw_bone_data = file_io.read_fmt(file, 'B3f3h', collections.namedtuple('raw_bone_data', 'parent, px, py, pz, rx, ry, rz'))
+                    
+                    qx = raw_bone_data.rx / self.ROTATION_DIVISOR
+                    qy = raw_bone_data.ry / self.ROTATION_DIVISOR
+                    qz = raw_bone_data.rz / self.ROTATION_DIVISOR
+                    qw = math.sqrt((1 - (qx ** 2) - (qy ** 2) - (qz ** 2)))
+
+                    # generate rotation
+                    rotation = mathutils.Quaternion((qw, qx, qy, qz))
+
+                    # generate positioon
+                    position = mathutils.Vector((
+                        raw_bone_data.px,
+                        raw_bone_data.py,
+                        raw_bone_data.pz
+                    ))
+
+                    bone_transform = self._bone_transform(rotation, position)
+                    bone = self._bone(bone_transform, raw_bone_data.parent)
+                    self.bones.append(bone)
+
+                for bone_index in range((header.root_bone_count + header.bone_count)):
+                    bone_name = file_io.read_nullstr(file)
+
+                    current_bone = self.bones[bone_index]
+                    current_bone.name = bone_name
+
+                    # transform bones by their parents to the correct place
+                    if current_bone.parent > -1:
+                        parent_bone = self.bones[current_bone.parent]
+                        current_bone.generate_world_transform_by_parent(parent_bone)
+
+                return True
+
+        except:
+            log.error_log(traceback.print_exc())
+            return False
--- src/assets/xmodelsurf.py
+++ src/assets/xmodelsurf.py
@@ -78,7 +78,7 @@ class XModelSurfV20:
         self.name = ''
         self.surfaces = []
 
-    def load(self, xmodel_surf: str, xmodel_part: xmodelpart.XModelPart = None) -> bool:
+    def load(self, xmodel_surf: str, xmodel_part: xmodelpart.XModelPartV20 = None) -> bool:
         self.name = os.path.basename(xmodel_surf)
         try:
             with open(xmodel_surf, 'rb') as file:
@@ -171,7 +171,7 @@ class XModelSurfV20:
                         triangle = file_io.read_fmt(file, '3H')
                         triangles.append(triangle)
 
-                    self.surfaces.append(self._surface(vertices,triangles))
+                    self.surfaces.append(self._surface(vertices, triangles))
 
                 return True
 
@@ -344,6 +344,161 @@ class XModelSurfV14:
 
                             vertices[i].weights[0].influence -= weight_influence
                             vertices[i].weights.append(self._weight(weight_bone, weight_influence))
+
+                    self.surfaces.append(self._surface(vertices, triangles))
+
+                return True
+
+        except:
+            log.error_log(traceback.print_exc())
+            return False
+
+"""
+XModelSurfV25 class represents xmodelsurf structure for CoD4
+"""
+class XModelSurfV25:
+
+    PATH = 'xmodelsurfs'
+    VERSION = 25
+    RIGGED = 65535
+
+    # --------------------------------------------------------------------------------------------
+    class _weight:
+        __slots__ = ('bone', 'influence')
+
+        def __init__(self, bone: int, influence: float) -> None:
+            self.bone = bone
+            self.influence = influence
+    
+    class _uv:
+        __slots__ = ('u', 'v')
+
+        def __init__(self, u: float, v: float) -> None:
+            self.u = u
+            self.v = v
+
+        def to_tuple(self) -> tuple:
+            return (self.u, self.v)
+    
+    class _color:
+        __slots__ = ('red', 'green', 'blue', 'alpha')
+
+        def __init__(self, red: int, green: int, blue: int, alpha: int) -> None:
+            self.red = red
+            self.green = green
+            self.blue = blue
+            self.alpha = alpha
+        
+        def to_tuple(self) -> tuple:
+            return (self.red, self.green, self.blue, self.alpha)
+    
+    class _vertex:
+        __slots__ = ('normal', 'color', 'uv', 'binormal', 'tangent', 'bone', 'position', 'weights')
+
+        def __init__(self, normal: mathutils.Vector , color: XModelSurfV20._color, uv: XModelSurfV20._uv, binormal: mathutils.Vector, tangent: mathutils.Vector, bone: int, position: mathutils.Vector, weights: list[XModelSurfV20._weight]) -> None:
+            self.normal = normal
+            self.color = color
+            self.uv = uv
+            self.binormal = binormal
+            self.tangent = tangent
+            self.bone = bone
+            self.position = position
+            self.weights = weights
+    
+    class _surface:
+        __slots__ = ('vertices', 'triangles')
+
+        def __init__(self, vertices: list[XModelSurfV20._vertex], triangles: list[tuple]) -> None:
+            self.vertices = vertices
+            self.triangles = triangles
+
+    # --------------------------------------------------------------------------------------------
+    
+    __slots__ = ('name', 'surfaces')
+
+    def __init__(self) -> None:
+        self.name = ''
+        self.surfaces = []
+
+    def load(self, xmodel_surf: str) -> bool:
+        self.name = os.path.basename(xmodel_surf)
+        try:
+            with open(xmodel_surf, 'rb') as file:
+                header = file_io.read_fmt(file, '2H', collections.namedtuple('header', 'version, surface_count'))
+                if header.version != self.VERSION:
+                    log.info_log(f"Xmodel version {header.version} is not supported!")
+                    return False
+                
+                for _ in range(header.surface_count):
+                    surface_header = file_io.read_fmt(file, '3x3H', collections.namedtuple('surface_header', 'vertex_count, triangle_count, vertex_count2'))
+                    if surface_header.vertex_count != surface_header.vertex_count2:
+                        while True:
+                            p = file_io.read_ushort(file)
+                            if p == 0:
+                                break
+                        
+                        file.read(2) # padding
+                    else:
+                        file.read(4) # padding
+                    
+                    vertices = []
+                    for _ in range(surface_header.vertex_count):
+
+                        vn = file_io.read_fmt(file, '3f')
+                        vertex_normal = mathutils.Vector((vn[0], vn[1], vn[2]))
+
+                        clr = file_io.read_fmt(file, '4B')
+                        vertex_color = self._color(
+                            clr[0] / 255,
+                            clr[1] / 255,
+                            clr[2] / 255,
+                            clr[3] / 255
+                        )
+
+                        uv = file_io.read_fmt(file, '2f')
+                        vertex_uv = self._uv(uv[0], 1 - uv[1]) # flip UV
+
+                        bn = file_io.read_fmt(file, '3f')
+                        vertex_binormal = mathutils.Vector((bn[0], bn[1], bn[2]))
+
+                        tn = file_io.read_fmt(file, '3f')
+                        vertex_tangent = mathutils.Vector((tn[0], tn[1], tn[2]))
+
+                        weight_count = 0
+                        vertex_bone = 0
+                        if surface_header.vertex_count != surface_header.vertex_count2:
+                            weight_count = file_io.read_uchar(file)
+                            vertex_bone = file_io.read_ushort(file)
+
+                        vp = file_io.read_fmt(file, '3f')
+                        vertex_position = mathutils.Vector((vp[0], vp[1], vp[2]))
+
+                        vertex_weights = []
+                        vertex_weights.append(self._weight(vertex_bone, 1.0))
+
+                        if weight_count > 0:
+                            for _ in range(weight_count):
+                                weight_bone = file_io.read_ushort(file)
+                                weight_influence = file_io.read_ushort(file)
+                                weight_influence = float(weight_influence / self.RIGGED)
+                                vertex_weights[0].influence -= weight_influence
+                                vertex_weights.append(self._weight(weight_bone, weight_influence))
+                        
+                        vertices.append(self._vertex(
+                            vertex_normal,
+                            vertex_color,
+                            vertex_uv,
+                            vertex_binormal,
+                            vertex_tangent,
+                            vertex_bone,
+                            vertex_position,
+                            vertex_weights
+                        ))
+
+                    triangles = []
+                    for _ in range(surface_header.triangle_count):
+                        triangle = file_io.read_fmt(file, '3H')
+                        triangles.append(triangle)
 
                     self.surfaces.append(self._surface(vertices, triangles))
 
