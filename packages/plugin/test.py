5129a3073dd166b2be619166b9ccfa4104392019
mauserzjeh <mauserzjeh@gmail.com>
2023-07-13 22:28:18 +0200

    we go rust baby

--- packages/plugin/test.py
+++
@@ -1,7 +0,0 @@
-from importer import importer
-import callbacks
-
-imp = importer.NewImporter(10, callbacks.callbackObj)
-assetPath = "E:/MOVIEMAKING/CALL OF DUTY/3D STUFF/CODASSETS/COD2ASSETS"
-filePath = f"{assetPath}/maps/mp/mp_toujane.d3dbsp"
-imp.ImportIBSP(assetPath=assetPath, filePath=filePath)
--- scripts/build_importer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mv ../go.work ../_go.work
-cd ../packages/importer
-gopy build -output=importer -vm=python importer
-rm -r ../plugin/importer
-mv importer ../plugin/importer
-cd ../../
-mv _go.work go.work
