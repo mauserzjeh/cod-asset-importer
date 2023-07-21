d284051b79322ffe9463181d8eff2cc92fb6146b
mauserzjeh <mauserzjeh@gmail.com>
2023-07-21 17:00:37 +0200

    measure duration for testing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use cod_asset_importer::assets::ibsp::Ibsp;
 use cod_asset_importer::{error_log, info_log};
-
+use std::time;
 
 fn main() {
-    let file_path = "E:\\MOVIEMAKING\\CALL OF DUTY\\3D STUFF\\CODASSETS\\COD2ASSETS\\maps\\mp\\mp_toujane.d3dbsp".to_string();
+    let file_path =
+        "E:\\MOVIEMAKING\\CALL OF DUTY\\3D STUFF\\CODASSETS\\COD2ASSETS\\maps\\toujane_ride.d3dbsp"
+            .to_string();
+    let start = time::Instant::now();
     let res = Ibsp::load(file_path);
+    let duration = start.elapsed();
     match res {
         Ok(ibsp) => {
             info_log!("name: {}", ibsp.name);
             info_log!("materials_len: {}", ibsp.materials.len());
             info_log!("entities_len: {}", ibsp.entities.len());
             info_log!("surfaces_len: {}", ibsp.surfaces.len());
-        },
+        }
         Err(error) => {
             error_log!("{}", error)
         }
     }
+
+    info_log!("duration {:?}", duration)
 }
