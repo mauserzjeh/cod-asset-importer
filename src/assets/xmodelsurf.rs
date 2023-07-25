497a9fd5d389c57c2e1b7fb59fe7967cfd0763c1
mauserzjeh <mauserzjeh@gmail.com>
2023-07-25 23:01:43 +0200

    v14 xmodelsurf loading

--- src/assets/xmodelsurf.rs
+++ src/assets/xmodelsurf.rs
@@ -3,7 +3,10 @@ use super::xmodelpart::XModelPart;
 use crate::utils::{
     binary,
     error::Error,
-    math::{Color, Triangle, Vec3, UV},
+    math::{
+        color_from_vec, uv_from_vec, vec3_add, vec3_from_vec, vec3_rotate, Color, Triangle, Vec3,
+        UV,
+    },
     path::file_name_without_ext,
     Result,
 };
@@ -31,7 +34,7 @@ pub struct XModelSurfWeight {
 
 pub struct XModelSurfSurface {
     vertices: Vec<XModelSurfVertex>,
-    triangles: Triangle,
+    triangles: Vec<Triangle>,
 }
 
 const RIGGED: i32 = 65535;
@@ -49,15 +52,15 @@ impl XModelSurf {
 
         match XModelVersion::valid(version) {
             Some(XModelVersion::V14) => {
-                xmodel_surf.load_v14(&mut file)?;
+                xmodel_surf.load_v14(&mut file, xmodel_part)?;
                 return Ok(xmodel_surf);
             }
             Some(XModelVersion::V20) => {
-                xmodel_surf.load_v20(&mut file)?;
+                xmodel_surf.load_v20(&mut file, xmodel_part)?;
                 return Ok(xmodel_surf);
             }
             Some(XModelVersion::V25) => {
-                xmodel_surf.load_v25(&mut file)?;
+                xmodel_surf.load_v25(&mut file, xmodel_part)?;
                 return Ok(xmodel_surf);
             }
             None => {
@@ -69,15 +72,137 @@ impl XModelSurf {
         }
     }
 
-    fn load_v14(&mut self, file: &mut File) -> Result<()> {
+    fn load_v14(&mut self, file: &mut File, xmodel_part: Option<XModelPart>) -> Result<()> {
+        let surface_count = binary::read::<u16>(file)?;
+
+        for _ in 0..surface_count {
+            binary::skip(file, 1)?;
+
+            let vertex_count = binary::read::<u16>(file)?;
+            let triangle_count = binary::read::<u16>(file)?;
+
+            binary::skip(file, 2)?;
+
+            let mut default_bone_idx = binary::read::<u16>(file)?;
+            if default_bone_idx as i32 == RIGGED {
+                binary::skip(file, 4)?;
+                default_bone_idx = 0
+            }
+
+            let mut triangles: Vec<Triangle> = Vec::new();
+            loop {
+                let idx_count = binary::read::<u8>(file)?;
+
+                let idx1 = binary::read::<u16>(file)?;
+                let mut idx2 = binary::read::<u16>(file)?;
+                let mut idx3 = binary::read::<u16>(file)?;
+
+                if idx1 != idx2 && idx1 != idx3 && idx2 != idx3 {
+                    triangles.push([idx1, idx2, idx3]);
+                }
+
+                let mut i = 3;
+                while i < idx_count {
+                    let idx4 = idx3;
+                    let idx5 = binary::read::<u16>(file)?;
+
+                    if idx4 != idx2 && idx4 != idx5 && idx2 != idx5 {
+                        triangles.push([idx4, idx2, idx5]);
+                    }
+
+                    let v = i + 1;
+                    if v > idx_count {
+                        break;
+                    }
+
+                    idx2 = idx5;
+                    idx3 = binary::read::<u16>(file)?;
+
+                    if idx4 != idx2 && idx4 != idx3 && idx2 != idx3 {
+                        triangles.push([idx4, idx2, idx3]);
+                    }
+
+                    i = v + 1;
+                }
+
+                if triangles.len() > triangle_count as usize {
+                    break;
+                }
+            }
+
+            let mut bone_weight_counts = vec![0u16; vertex_count as usize];
+            let mut vertices: Vec<XModelSurfVertex> = Vec::new();
+            for i in 0..vertex_count {
+                let normal = binary::read_vec::<f32>(file, 3)?;
+                let mut normal = vec3_from_vec(normal).unwrap();
+
+                let uv = binary::read_vec::<f32>(file, 2)?;
+                let mut weight_count = 0;
+                let mut vertex_bone_idx = default_bone_idx;
+
+                if default_bone_idx as i32 == RIGGED {
+                    weight_count = binary::read::<u16>(file)?;
+                    vertex_bone_idx = binary::read::<u16>(file)?;
+                }
+
+                let position = binary::read_vec::<f32>(file, 3)?;
+                let mut position = vec3_from_vec(position).unwrap();
+
+                if weight_count != 0 {
+                    binary::skip(file, 4)?;
+                }
+
+                bone_weight_counts[i as usize] = weight_count;
+
+                if let Some(xmodel_part) = xmodel_part.to_owned() {
+                    let xmodel_part_bone = xmodel_part.bones[vertex_bone_idx as usize].to_owned();
+
+                    position = vec3_rotate(position, xmodel_part_bone.world_transform.rotation);
+                    position = vec3_add(position, xmodel_part_bone.world_transform.position);
+                    normal = vec3_rotate(normal, xmodel_part_bone.world_transform.rotation);
+                }
+
+                vertices.push(XModelSurfVertex {
+                    normal,
+                    color: [1.0, 1.0, 1.0, 1.0],
+                    uv: uv_from_vec(uv).unwrap(),
+                    bone: vertex_bone_idx,
+                    position,
+                    weights: vec![XModelSurfWeight {
+                        bone: vertex_bone_idx,
+                        influence: 1.0,
+                    }],
+                })
+            }
+
+            for i in 0..vertex_count {
+                for _ in 0..bone_weight_counts[i as usize] {
+                    let weight_bone_idx = binary::read::<u16>(file)?;
+
+                    binary::skip(file, 12)?;
+
+                    let mut weight_influence = binary::read::<f32>(file)?;
+                    weight_influence /= RIGGED as f32;
+
+                    vertices[i as usize].weights[0].influence -= weight_influence;
+                    vertices[i as usize].weights.push(XModelSurfWeight {
+                        bone: weight_bone_idx,
+                        influence: weight_influence,
+                    });
+                }
+            }
+
+            self.surfaces.push(XModelSurfSurface { vertices, triangles });
+        }
+
         Ok(())
     }
 
-    fn load_v20(&mut self, file: &mut File) -> Result<()> {
+    fn load_v20(&mut self, file: &mut File, xmodel_part: Option<XModelPart>) -> Result<()> {
         Ok(())
     }
 
-    fn load_v25(&mut self, file: &mut File) -> Result<()> {
+    fn load_v25(&mut self, file: &mut File, xmodel_part: Option<XModelPart>) -> Result<()> {
         Ok(())
     }
 }
